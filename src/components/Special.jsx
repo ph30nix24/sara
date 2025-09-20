c4cb1163fab4c20ee27f8c01d9ed81cb3de1c430
Anuj <starlordindia1230@gmail.com>
2025-09-20 22:25:11 +0530

    initial deploy

--- src/components/Special.jsx
+++ src/components/Special.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 const Special = () => {
   return (
     <div className='w-full md:h-[500px] h-[320px] relative'>
-        <img src="/assets/img/offer.jpg" className='size-full object-center object-cover absolute z-10' alt="" />
+        <img src="./assets/img/offer.jpg" className='size-full object-center object-cover absolute z-10' alt="" />
         <div className='absolute z-20 top-0 left-0 w-full h-full bg-black opacity-50'></div>
         <div className='relative z-30 w-full h-full element-center flex-col'>
             <h1 className='text-4xl md:text-6xl font-semibold text-white uppercase tracking-wide'>Special offer</h1>
